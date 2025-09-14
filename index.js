9627e27d83f95d6dada49efc0c49010415a3f58d
Geon Joseph <geonjoseph1805@gmail.com>
2025-09-14 08:53:09 +0530

    Initial commit – Drum Kit project

---
+++ index.js
@@ -0,0 +1,43 @@
+// Select all buttons with class "drum"
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    const buttonInnerHTML = this.innerHTML;
+    playSound(buttonInnerHTML);
+  });
+}
+
+// Optional: Add keyboard support
+document.addEventListener("keydown", function(event) {
+  playSound(event.key);
+});
+
+// Function to play sound based on key/button
+function playSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+    default:
+      console.log("Unrecognized key:", key);
+  }
+}
